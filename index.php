7712a239ab8191a5a73c7f59e40224b3e1f44fff
eyngelcondense <angelmaeramirezcortino@gmail.com>
2026-05-02 10:25:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>CI/CD Demo</title>
-</head>
-<body style="background-color:green">
-    <h1 id="title">Hello CI/CD rtials</h1>
-
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCtbwmxsM1LCh9SK1zGhDpeAFKSjkmFcsGNg&s" width="150" height="150">
-<h1>This is triggered by a push </h1>
+<body style="background-color:pink">
+    <h1><?php
+        echo "hello world"; 
+    ?></h1>
 </body>
-</html>
